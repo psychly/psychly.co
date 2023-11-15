d8fff425309eb8b5594fbee3280342d34994bcc6
psychly <94637813+psychly@users.noreply.github.com>
2023-11-15 08:03:41 -0800

    Update browse-map.js

--- browse-map.js
+++ browse-map.js
@@ -1,80 +1,71 @@
-// dynamic google map and markers that react to the search and filters on the browse page
-window.addEventListener('load', function () {
-  setTimeout(function() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      maxZoom: 14,
-      scrollwheel: true
-    });
-
-    // Function to update map markers
-    window.updateMarkers = function() {
-      // Clear out the old markers if they exist.
-      if (window.markers) {
-        window.markers.forEach(function(marker) {
-          marker.setMap(null);
-        });
-      }
-
-      var locations = [];
-      var dynPlaces = document.querySelectorAll('.collection');
-      dynPlaces.forEach(function(elem) {
-        var place = [];
-        var pName = elem.querySelector('.practice-name').innerText;
-        var pLat = Number(elem.querySelector('.lat').innerText);
-        var pLong = Number(elem.querySelector('.long').innerText);
-
-        place.push(pName, pLat, pLong);
-        locations.push(place);
+// dynamic google map and markers that reacts to the search and filters on the browse page
+  window.addEventListener('load', function () {
+    setTimeout(function() {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        maxZoom: 14,
+        scrollwheel: true
       });
 
-      var bounds = new google.maps.LatLngBounds();
-      locations.forEach(function(location) {
-        var latlng = new google.maps.LatLng(location[1], location[2]);
-        bounds.extend(latlng);
-      });
+      // Function to update map markers
+      window.updateMarkers = function() {
+        // Clear out the old markers if they exist.
+        if (window.markers) {
+          window.markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+        }
 
-      // Only adjust the bounds if there are markers.
-      if (locations.length > 0) {
-        map.fitBounds(bounds);
-      }
+        var locations = [];
+        var dynPlaces = document.querySelectorAll('.collection');
+        dynPlaces.forEach(function(elem) {
+          var place = [];
+          var pName = elem.querySelector('.practice-name').innerText;
+          var pLat = Number(elem.querySelector('.lat').innerText);
+          var pLong = Number(elem.querySelector('.long').innerText);
 
-      var infowindow = new google.maps.InfoWindow();
-
-      // Store markers globally to access them for removal later
-      window.markers = locations.map(function(location) {
-        var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(location[1], location[2]),
-          map: map,
-          title: location[0]
+          place.push(pName, pLat, pLong);
+          locations.push(place);
         });
 
-        marker.addListener('click', function() {
-          infowindow.setContent(location[0]);
-          infowindow.open(map, marker);
+        var bounds = new google.maps.LatLngBounds();
+        locations.forEach(function(location) {
+          var latlng = new google.maps.LatLng(location[1], location[2]);
+          bounds.extend(latlng);
         });
 
-        return marker;
-      });
-    };
+        // Only adjust the bounds if there are markers.
+        if (locations.length > 0) {
+          map.fitBounds(bounds);
+        }
 
-    // Function to reset the map view
-    window.resetMap = function() {
+        var infowindow = new google.maps.InfoWindow();
+
+        // Store markers globally to access them for removal later
+        window.markers = locations.map(function(location) {
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(location[1], location[2]),
+            map: map,
+            title: location[0]
+          });
+
+          marker.addListener('click', function() {
+            infowindow.setContent(location[0]);
+            infowindow.open(map, marker);
+          });
+
+          return marker;
+        });
+      };
+
+      // Function to reset the map view
+      window.resetMap = function() {
+        // Call updateMarkers to reset markers on the map
+        updateMarkers();
+      };
+
+      // Initial call to update markers
       updateMarkers();
-    };
 
-    // Initial call to update markers
-    updateMarkers();
-
-    // Check for URL changes periodically
-    var lastUrl = location.href; 
-    setInterval(function() {
-      var currentUrl = location.href;
-      if (currentUrl !== lastUrl) {
-        lastUrl = currentUrl;
-        updateMarkers(); // Update markers if the URL has changed
-      }
-    }, 1000); // Check every second
-
-  }, 500);
-}, false);
+    }, 500);
+  }, false);
