ab7b2593d01287dfb8f49eb62dc305d6dfa468c6
psychly <94637813+psychly@users.noreply.github.com>
2023-12-11 21:30:09 -0800

    Create claim-listing-contact.js

---
+++ claim-listing-contact.js
@@ -0,0 +1,17 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const itemId = urlParams.get('itemID');
+
+    // Replace with your actual Cloudflare Worker URL
+    const workerEndpoint = `https://psychly-claim.tobcsmith.workers.dev/?itemID=${itemId}`;
+
+    fetch(workerEndpoint)
+      .then(response => response.json())
+      .then(data => {
+        // Populate all elements with the 'emailField' class
+        document.querySelectorAll('.emailField').forEach(el => el.textContent = data.email);
+        // Populate all elements with the 'phoneField' class
+        document.querySelectorAll('.phoneField').forEach(el => el.textContent = data.phone);
+      })
+      .catch(error => console.error('Error:', error));
+  });
