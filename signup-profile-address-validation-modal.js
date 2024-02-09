27a62c68e0411bef576985eaac27c56f8647e901
psychly <94637813+psychly@users.noreply.github.com>
2024-02-09 14:09:14 -0800

    Update signup-profile-address-validation-modal.js

--- signup-profile-address-validation-modal.js
+++ signup-profile-address-validation-modal.js
@@ -1,19 +1,19 @@
 // Function to unhide the 'profile-address-valid-container'
   function unhideAddressContainer() {
-    var container = document.getElementById('profile-address-valid-container');
+    var container = document.getElementById('signup-profile-address-valid-container');
     container.style.display = 'block'; // This will unhide the container
   }
 
   //Function to hide the 'profile-address-valid-container'
   function hideAddressContainer() {
-    var container = document.getElementById('profile-address-valid-container');
+    var container = document.getElementById('signup-profile-address-valid-container');
     container.style.display = 'none'; // This will hide the container
   }
 
   // Event listeners setup function
   function setupEventListeners() {
     // Listener for 'confirm-address-button'
-    document.getElementById('confirm-address-button').addEventListener('click', unhideAddressContainer);
+    document.getElementById('signup-confirm-address-button').addEventListener('click', unhideAddressContainer);
 
     // Listeners for 'close-button' and 'close-button-back'
     var closeButtons = document.querySelectorAll('.close_button, .close-button-back');
@@ -22,11 +22,11 @@
     });
 
     // Listener for 'profile-save-address-button'
-    var saveAddressButton = document.getElementById('profile-save-address-button');
+    var saveAddressButton = document.getElementById('signup-profile-save-address-button');
     if (saveAddressButton) {
       saveAddressButton.addEventListener('click', function() {
         // Replace 'original-submit-button-id' with the actual ID or class of the MemberStack form's submit button
-        var originalSubmitButton = document.getElementById('address-submit-button-hidden') || document.querySelector('.original-submit-button-class');
+        var originalSubmitButton = document.getElementById('signup-address-submit-button-hidden') || document.querySelector('.original-submit-button-class');
         if (originalSubmitButton) {
           originalSubmitButton.click(); // Simulate a click on the original submit button
         }
