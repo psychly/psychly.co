0a884aaba22405d2932f1ab537638714cbf058f0
psychly <94637813+psychly@users.noreply.github.com>
2024-04-07 08:54:52 -0700

    Update profile-image-uploader.js

--- profile-image-uploader.js
+++ profile-image-uploader.js
@@ -8,7 +8,7 @@ function setupUploadWidget(buttonId, memberStackField) {
       showAdvancedOptions: false, // Optional
       cropping: true, // Optional
       multiple: false, // Optional
-      //maxFileSize: 5000000,
+      maxFileSize: 500000,
       defaultSource: 'local', // Optional
       styles: {
         palette: {
