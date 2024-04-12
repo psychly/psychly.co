d326adad9e3a6ffafab55ede208ec9f98640bb74
psychly <94637813+psychly@users.noreply.github.com>
2024-04-11 19:31:43 -0700

    Update profile-image-uploader.js

--- profile-image-uploader.js
+++ profile-image-uploader.js
@@ -1,4 +1,4 @@
-function setupUploadWidget(buttonId, memberStackField) {
+function setupUploadWidget(buttonId, memberStackField, imageId) {
   document.getElementById(buttonId).addEventListener("click", function(event) {
     cloudinary.openUploadWidget({
       cloudName: 'dfj3b96gg',
@@ -40,17 +40,64 @@ function setupUploadWidget(buttonId, memberStackField) {
     (error, result) => {
       if (!error && result && result.event === "success") {
         console.log('Uploaded Image URL:', result.info.secure_url);
-        document.querySelector('[data-ms-member="' + memberStackField + '"]').value = result.info.secure_url;
+        var imageURL = result.info.secure_url;
+        document.getElementById(memberStackField).value = imageURL;
+        updateImage(imageId, imageURL);
       }
     });
   });
 }
 
+function updateImage(imageId, imageURL) {
+  var dynamicImageDiv = document.getElementById(imageId);
+  dynamicImageDiv.style.backgroundImage = "url('" + imageURL + "')";
+  dynamicImageDiv.style.backgroundSize = "contain";
+  dynamicImageDiv.style.backgroundPosition = "center";
+  dynamicImageDiv.style.backgroundRepeat = "no-repeat";
+  dynamicImageDiv.style.height = "160px";
+  dynamicImageDiv.style.width = "100%";
+  dynamicImageDiv.style.maxWidth = "100%";
+}
+
+function loadImages() {
+  setTimeout(function() {
+    var inputFields = [
+      "images-input-main",
+      "images-input-setting",
+      "images-input-1",
+      "images-input-2",
+      "images-input-3",
+      "images-input-4",
+      "images-input-5"
+    ];
+    
+    var imageIds = [
+      "image-main-id",
+      "image-setting-id",
+      "image-other-1-id",
+      "image-other-2-id",
+      "image-other-3-id",
+      "image-other-4-id",
+      "image-other-5-id"
+    ];
+    
+    for (var i = 0; i < inputFields.length; i++) {
+      var imageURL = document.getElementById(inputFields[i]).value;
+      if (imageURL) {
+        updateImage(imageIds[i], imageURL);
+      }
+    }
+  }, 500);
+}
+
 // Set up each upload widget
-setupUploadWidget("upload_main_image", "main-headshot-image");
-setupUploadWidget("upload_setting_image", "main-setting-image");
-setupUploadWidget("upload_other_image_1", "other-image-1");
-setupUploadWidget("upload_other_image_2", "other-image-2");
-setupUploadWidget("upload_other_image_3", "other-image-3");
-setupUploadWidget("upload_other_image_4", "other-image-4");
-setupUploadWidget("upload_other_image_5", "other-image-5");
+setupUploadWidget("upload_main_image", "images-input-main", "image-main-id");
+setupUploadWidget("upload_setting_image", "images-input-setting", "image-setting-id");
+setupUploadWidget("upload_other_image_1", "images-input-1", "image-other-1-id");
+setupUploadWidget("upload_other_image_2", "images-input-2", "image-other-2-id");
+setupUploadWidget("upload_other_image_3", "images-input-3", "image-other-3-id");
+setupUploadWidget("upload_other_image_4", "images-input-4", "image-other-4-id");
+setupUploadWidget("upload_other_image_5", "images-input-5", "image-other-5-id");
+
+// Load images after a delay of 0.5 seconds
+document.addEventListener("DOMContentLoaded", loadImages);
