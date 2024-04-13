821d68a5940b283a568babbf6c6fa39bc889a816
psychly <94637813+psychly@users.noreply.github.com>
2024-04-13 14:17:53 -0700

    Update phone-number-auto-detect.js

--- phone-number-auto-detect.js
+++ phone-number-auto-detect.js
@@ -1,32 +1,36 @@
-
 // 💙 MEMBERSCRIPT #41 v0.2 💙 PERFECT PHONE NUMBER INPUTS (WITH IP LOOKUP)
-  $(document).ready(function() {
-    $('input[ms-code-phone-number]').each(function() {
-      var input = this;
-      var preferredCountries = $(input).attr('ms-code-phone-number').split(',');
 
-      var iti = window.intlTelInput(input, {
-        preferredCountries: preferredCountries,
-        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
-      });
+$(document).ready(function() {
+  $('input[ms-code-phone-number]').each(function() {
+    var input = this;
+    var preferredCountries = $(input).attr('ms-code-phone-number').split(',');
 
-      $.get("https://ipinfo.io", function(response) {
-        var countryCode = response.country;
-        iti.setCountry(countryCode);
-      }, "jsonp");
-
-      input.addEventListener('change', formatPhoneNumber);
-      input.addEventListener('keyup', formatPhoneNumber);
-
-      function formatPhoneNumber() {
-        var formattedNumber = iti.getNumber(intlTelInputUtils.numberFormat.INTERNATIONAL);
-        input.value = formattedNumber;
-      }
-
-      var form = $(input).closest('form');
-      form.submit(function() {
-        var formattedNumber = iti.getNumber(intlTelInputUtils.numberFormat.INTERNATIONAL);
-        input.value = formattedNumber;
-      });
+    var iti = window.intlTelInput(input, {
+      preferredCountries: preferredCountries,
+      utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
     });
+
+    $.get("https://ipinfo.io", function(response) {
+      var countryCode = response.country;
+      iti.setCountry(countryCode);
+    }, "jsonp");
+
+    input.addEventListener('change', formatPhoneNumber);
+    input.addEventListener('keyup', formatPhoneNumber);
+
+    function formatPhoneNumber() {
+      var formattedNumber = iti.getNumber(intlTelInputUtils.numberFormat.INTERNATIONAL);
+      input.value = formattedNumber;
+    }
+
+    var form = $(input).closest('form');
+    form.submit(function() {
+      var formattedNumber = iti.getNumber(intlTelInputUtils.numberFormat.INTERNATIONAL);
+      input.value = formattedNumber;
+    });
+
+    // Set the z-index of the dropdown menu to 9999
+    var dropdown = $(input).next('.iti__country-list');
+    dropdown.css('z-index', '9999');
   });
+});
